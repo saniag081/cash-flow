64c2ad8e3588c11f7532b32e8c0d867e03e21454
Santiago G <sgonzalez@modyo.com>
2022-04-01 23:06:37 -0500

    feat(Layout.vue): se agrega componente

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,3 +1,20 @@
 <template>
-    <div>Hola Mundo</div>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+    </Layout>
 </template>
+
+<script>
+import Layout from '../Layout.vue';
+import Header from '../Header.vue';
+
+
+export default {
+    components: {
+        Layout,
+        Header,
+    },
+};
+</script>
